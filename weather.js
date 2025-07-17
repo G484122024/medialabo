de84bfd89a05f39167c84f1b82f9caea0d9c7b28
G484122024 <248412@st.takushoku-u.ac.jp>
2025-07-17 16:47:30 +0900

    課題６−１

--- weather.js
+++ weather.js
@@ -11,6 +11,7 @@ function print(data) {
 }
 
 // 課題5-1 の関数 printDom() はここに記述すること
+
 function printDom(data) {
   let oldResult = document.querySelector("#result");
   if (oldResult) oldResult.remove();
@@ -22,6 +23,14 @@ function printDom(data) {
   let name = document.createElement("p");
   name.textContent = "都市名: " + data.name;
   resultDiv.appendChild(name);
+
+  let lon = document.createElement("p");
+  lon.textContent = "経度: " + data.coord.lon;
+  resultDiv.appendChild(lon);
+
+  let lat = document.createElement("p");
+  lat.textContent = "緯度: " + data.coord.lat;
+  resultDiv.appendChild(lat);
 
   let weather = document.createElement("p");
   weather.textContent = "天気: " + data.weather[0].description;
@@ -57,15 +66,28 @@ btn.addEventListener("click", sendRequest);
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
-  let input = document.querySelector("#searchBox").value;
+  let input = document.querySelector("#searchBox").value.trim();
   console.log("検索キー:", input);
+
+  const url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${input}.json`;
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) throw new Error("データの取得に失敗しました");
+      return response.json();
+    })
+    .then(showResult)
+    .catch(showError)
+    .finally(finish);
 }
 
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
-
+  print(resp);
+  printDom(resp);
 }
+
 
 // 課題6-1: 通信エラーが発生した時の処理
 function showError(err) {
@@ -77,54 +99,17 @@ function finish() {
     console.log('Ajax 通信が終わりました');
 }
 
+// 都市選択メニューのクリック処理
+document.querySelectorAll("#cityMenu li").forEach(item => {
+  item.addEventListener("click", () => {
+    const cityId = item.dataset.id;
+    document.querySelector("#searchBox").value = cityId;
+    sendRequest(); // 入力欄にIDを入れて検索実行
+  });
+});
+
 ////////////////////////////////////////
 // 以下はグルメのデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
 // 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
 
